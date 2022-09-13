6773612a7b134824ef57fb83ae5509593ccd2e03
john sjödén <johnsjoden95@gmail.com>
2022-09-13 10:20:01 +0200

    first commit

---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchData } from '../api'
+const parse = require("html-react-parser")
+
+export default function DetailPage() {
+  const [data, setResult] = useState([])
+  const id = useParams().id
+  const url = process.env.REACT_APP_NOT_SECRET_CODE
+  useEffect(() => {
+    fetchData(`${url}/${id}`)
+    .then(data => {
+      setResult(data)
+    })
+  }, [])
+  console.log(data)
+  return (
+    <div>
+      <h2>{data.slug}</h2>
+      <p>{data.content && parse(data.content)}</p>
+    </div>
+  )
+}
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import {fetchData} from "../api"
+import { Link } from 'react-router-dom'
+export default function HomePage() {
+  const [result, setResult] = useState([])
+  const url = process.env.REACT_APP_NOT_SECRET_CODE
+  console.log(url)
+  useEffect(() => {
+    fetchData(url).then(data => {
+      setResult(data.posts)
+    })
+  }, [])
+  return (
+    <div>
+      <h1>Click on a link to get to a detail page</h1>
+      {result ? result.map((item, index) => {
+        return <div key={index}>
+          <Link to={`/${item.ID}`}>Link to detail about {item.slug}</Link>
+        </div>
+      }) : "not working"}
+    </div>
+  )
+}
