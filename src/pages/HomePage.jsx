66900a6e3a712862ba9e5d37dd12c382cc0fcd1b
john sjödén <johnsjoden95@gmail.com>
2022-09-13 11:25:11 +0200

    comon

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import {fetchData} from "../api"
 import { Link } from 'react-router-dom'
 export default function HomePage() {
-  const [result, setResult] = useState(null)
+  const [result, setResult] = useState([])
   const url = process.env.REACT_APP_API_URL
   console.log(url)
   useEffect(() => {
-    fetch(url)
+    fetch(`${url}/posts`)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setResult(data.posts))
   }, [])
   return (
     <div>
       <h1>Click on a link to get to a detail page</h1>
-      {result ? result.map((item, index) => {
+      {result.map((item, index) => {
         return <div key={index}>
           <Link to={`/${item.ID}`}>Link to detail about {item.slug && item.slug}</Link>
         </div>
-      }) : "not working"}
+      }) }
     </div>
   )
 }
